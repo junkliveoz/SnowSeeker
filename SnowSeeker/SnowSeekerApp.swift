be4bb1316c4cf12293b03d6574d670e6c8cfc05d
Adam Sayer <asayer82@icloud.com>
2024-09-06 09:06:47 +1000

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Adam on 6/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
