c9a13e2edd63b1246a689f6b5bf2d1d2ca372181
Adam Sayer <asayer82@icloud.com>
2024-09-06 16:03:02 +1000

    Completed Day 97

---
+++ SnowSeeker/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  SnowSeeker
+//
+//  Created by Adam on 6/9/2024.
+//
+
+import Foundation
---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,8 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Adam on 6/9/2024.
+//
+
+import Foundation
